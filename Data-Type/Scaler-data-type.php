fad6a38e2cf803435f78557eac3191a87361e446
tanjil-hosain <tanjilh85@gmail.com>
2026-03-06 21:10:21 +0600

    string data-type

---
+++ Data-Type/Scaler-data-type.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Scaler_Data_type</title>
+</head>
+<body>
+    <?php
+    // String Data Type
+    $data = "Please Quit This Earth";
+    echo $data;
+    echo "<br>";
+    echo var_dump($data);
+    ?>
+    
+</body>
+</html>
