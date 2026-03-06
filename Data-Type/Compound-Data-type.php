80b8e69130016c04538d87dbc2d6ea250ac5d111
tanjil-hosain <tanjilh85@gmail.com>
2026-03-06 21:44:19 +0600

    Array

---
+++ Data-Type/Compound-Data-type.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Compound Data Type</title>
+</head>
+<body>
+    <?php
+    // Array
+     $student = array("akib","sakib","rakin");
+    echo $student;
+    echo"<br>";
+    var_dump($student);
+    ?>
+</body>
+</html>
