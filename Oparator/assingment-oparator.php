a68db7f9d414975f1aff44162cad3f005eee9417
tanjil-hosain <tanjilh85@gmail.com>
2026-03-07 21:04:15 +0600

    assingment-oparator

---
+++ Oparator/assingment-oparator.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Assingmet</title>
+</head>
+<body>
+   <h1>
+    The summation Is <br>
+     <?php
+    $x=20;
+    $x+=30;
+    echo $x;
+
+    ?>
+   </h1>
+</body>
+</html>
