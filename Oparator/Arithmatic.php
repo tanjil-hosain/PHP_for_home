61c980ad91f6233bb280f6dea48147099074f8f7
tanjil-hosain <tanjilh85@gmail.com>
2026-03-08 21:05:46 +0600

    arithmatic

---
+++ Oparator/Arithmatic.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Araithmatic Oparator</h1>
+    <?php
+    $x=10;
+    $y=20;
+
+    echo $x+$y;
+
+    echo "<br>";
+
+    echo $x*$y;
+
+    ?>
+
+     <?php
+    $x=10;
+    $y=20;
+
+    echo $x+$y;
+
+    echo "<br>";
+
+    echo $x*$y;
+
+    ?>
+</body>
+</html>
