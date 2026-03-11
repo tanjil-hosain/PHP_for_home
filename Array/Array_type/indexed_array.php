1854376c8c6dff3ed4474e6cf02b62ef86d7d3ea
tanjil-hosain <tanjilh85@gmail.com>
2026-03-11 22:16:31 +0600

    indexed Array

---
+++ Array/Array_type/indexed_array.php
@@ -0,0 +1,34 @@
+<?php
+//Index Array(Automatically or manually)
+
+//Automatically
+$cars = array("Volvo", "BMW", "Toyota");
+var_dump($cars);
+echo $cars[0];
+echo $cars[1];
+echo $cars[2];
+
+// $cars =["Volvo", "BMW", "Toyota"];
+
+
+// Display all array items:
+
+$cars = array("Volvo", "BMW", "Toyota");
+foreach ($cars as $x) {
+  echo "$x <br>";
+}
+
+
+$pen = ["Parker", "Flair", "Cello"];
+echo $pen[2];
+
+echo "<br>";
+// manually
+$cars = [];
+// $cars = array();//alternative way
+$cars[0] = "Volvo";
+$cars[1] = "BMW";
+$cars[2] = "Toyota";
+
+var_dump($cars);
+echo $cars[1];
