6811adfc4a2953f4c8ffd027c0d767ca59f20311
tanjil-hosain <tanjilh85@gmail.com>
2026-03-17 21:43:39 +0600

    file_get_contents

--- File-Oparating-system/file-read.php
+++ File-Oparating-system/file-read.php
@@ -2,3 +2,6 @@
 $result = fopen("text-file/first.txt", "r") or die("The File is not found");
 echo fread($result, filesize("text-file/first.txt"));
 fclose($result);
+
+echo"<br>";
+echo readfile("text-file/first.txt"); // ertar maddome korle charecter size porjonto asbe!
---
+++ File-Oparating-system/file_get_contents.php
@@ -0,0 +1,3 @@
+<?php
+$result = file_get_contents("text-file/first.txt");
+echo $result;       // etar maddhome short e text file k read kora jay
