c647291bf9677bd832092f4087099885de2b2a72
tanjil-hosain <tanjilh85@gmail.com>
2026-03-19 10:12:52 +0600

    associative array

--- Md-1/Array_type/associative-array.php
+++ Md-1/Array_type/associative-array.php
@@ -1,5 +1,10 @@
 <?php
-$person = ['Name' => 'Ifty', 'age' => 15, 'round' => '70']; // associative array k => ei key define korte hoy
+$person = ['name' => 'Ifty', 'age' => 15, 'round' => '70']; // associative array k => ei key define korte hoy
 
-echo "Student name ". $person["Name"]; // etay index er vodle key value er name dore call kora hoy!1
+echo "Student name ". $person["name"];  // etay index er vodle key value er name dore call kora hoy!1
+ echo "<br>";
+foreach($person as $key => $val) {
+    echo $key ." is = ". $val  . "<br>";  // evabe purota dekhano jay
+    
+}
 ?>
