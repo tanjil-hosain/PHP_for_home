e6bf12dc3cf81e14cad2963972adf94a09392c81
tanjil-hosain <tanjilh85@gmail.com>
2026-03-19 09:57:51 +0600

    associative array

---
+++ Md-1/Array_type/associative-array.php
@@ -0,0 +1,5 @@
+<?php
+$person = ['Name' => 'Ifty', 'age' => 15, 'round' => '70']; // associative array k => ei key define korte hoy
+
+echo "Student name ". $person["Name"]; // etay index er vodle key value er name dore call kora hoy!1
+?>
