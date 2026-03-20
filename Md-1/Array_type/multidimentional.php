b65f73da868b26e62cc17c107b186d9418cb8d66
tanjil-hosain <tanjilh85@gmail.com>
2026-03-20 11:10:37 +0600

    multidimentinal array

---
+++ Md-1/Array_type/multidimentional.php
@@ -0,0 +1,11 @@
+<?php
+$array= [
+    [1,3,5],
+    [10,20,30],
+    [100, 200, 300]
+];
+echo $array[2][2]; // 300  multidaimentinal array te 2 ti index number diye ber korte hoye 1st index number hocche array gula er r 2nd e ta hocche array er vitor er item
+echo "<br>";
+echo $array[1][0]; // 10
+
+?>
