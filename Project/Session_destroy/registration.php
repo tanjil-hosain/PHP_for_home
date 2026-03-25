f6fed4231638a4eac9e7d59cc753db2c007f775c
tanjil-hosain <tanjilh85@gmail.com>
2026-03-25 21:22:16 +0600

    form

---
+++ Project/Session_destroy/registration.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+    body {
+      /* background: linear-gradient(135deg, #544a5f, #252e3c); */
+      height: 100vh;
+    }
+    .form-container {
+      max-width: 400px;
+      width: 100%;
+      background: #ffffff;
+      padding: 30px;
+      border-radius: 15px;
+      box-shadow: 0 10px 25px rgba(0,0,0,0.2);
+    }
+  </style>
+</head>
+<body>
+      <div class="d-flex justify-content-center align-items-center vh-100">
+    <div class="form-container">
+      <h3 class="text-center mb-4">Register</h3>
+
+      <form>
+        <div class="mb-3">
+          <label class="form-label">Full Name</label>
+          <input type="text" class="form-control" placeholder="Enter your name" name="name" required>
+        </div>
+
+        <div class="mb-3">
+          <label class="form-label">Email address</label>
+          <input type="email" class="form-control" placeholder="Enter your email" name="email" required>
+        </div>
+
+        <div class="mb-3">
+          <label class="form-label">Password</label>
+          <input type="password" class="form-control" placeholder="Enter password" name="password" required>
+        </div>
+
+        <button type="submit" class="btn btn-primary w-100" name="register">Register</button>
+      </form>
+
+      <p class="text-center mt-3 mb-0">
+        Already have an account? <a href="#">Login</a>
+      </p>
+    </div>
+  </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
