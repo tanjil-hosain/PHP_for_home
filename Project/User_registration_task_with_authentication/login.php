ec1949f6e009de9b970c2e58f215613eb879b25a
tanjil-hosain <tanjilh85@gmail.com>
2026-04-09 22:21:13 +0600

    login form

--- Project/User_registration_task_with_authentication/login.php
+++ Project/User_registration_task_with_authentication/login.php
@@ -1,11 +1,51 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+    body {
+      height: 100vh;
+    }
+    .form-container {
+      max-width: 400px;
+      width: 100%;
+      background: #ffffff;
+      padding: 30px;
+      border-radius: 15px;
+      box-shadow: 0 10px 25px rgba(0,0,0,0.2);
+    }
+  </style>
 </head>
 <body>
-    Hi
+      <div class="d-flex justify-content-center align-items-center vh-100">
+    <div class="form-container">
+      <h3 class="text-center mb-4">User Login</h3>
+
+      <form method="post">
+
+        <div class="mb-3">
+          <label class="form-label">User name </label>
+          <input type="text" class="form-control" placeholder="User Name " name="uer_name" required>
+        </div>
+
+        <div class="mb-3">
+          <label class="form-label">Password</label>
+          <input type="password" class="form-control" placeholder="Enter password" name="password" required>
+        </div>
+
+        <button type="submit" class="btn btn-primary w-100" name="loginbtn">Login</button>
+      </form>
+
+      <p class="text-center mt-3 mb-0">
+       You don't have an account? <a href="registration.php">Register</a>
+      </p>
+    </div>
+  </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
